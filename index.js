aa8aa164ed4f4c574be0cabc1c08145a5bb3d0c7
jameszaman <james.hedayet@gmail.com>
2022-07-06 11:32:43 +0600

    Handling error for wrong commands.

--- index.js
+++ index.js
@@ -4,8 +4,14 @@
 const fs = require("fs");
 
 if (process.argv.length > 2) {
-  const func = require(`${__dirname}/functions/${process.argv[2]}`);
-  func(process.argv.slice(3));
+  try {
+    const func = require(`${__dirname}/functions/${process.argv[2]}`);
+    func(process.argv.slice(3));
+  } catch (e) {
+    console.log(
+      "Please provide a proper command.\nUse 'list' command to see all available functions commands."
+    );
+  }
 } else {
   console.log(
     "Hello! I am Sabestian, you're personal butler!\nUse 'list' command to see all available functions commands."
