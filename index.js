0546b70251474d5b4a93a3fbfa4ee0285acef552
jameszaman <james.hedayet@gmail.com>
2022-07-06 11:26:16 +0600

    Improved the performance by only requiring the command function.

--- index.js
+++ index.js
@@ -3,21 +3,11 @@
 // Imports
 const fs = require("fs");
 
-fs.readdir(`${__dirname}/functions`, function (err, files) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const functionObject = {};
-  files.forEach(function (file) {
-    functionObject[file] = require(`${__dirname}/functions/${file}`);
-  });
-
-  if (process.argv.length > 2) {
-    functionObject[process.argv[2]](process.argv.slice(3));
-  } else {
-    console.log(
-      "Hello! I am Sabestian, you're personal butler!\nUse 'list' command to see all available functions commands."
-    );
-  }
-});
+if (process.argv.length > 2) {
+  const func = require(`${__dirname}/functions/${process.argv[2]}`);
+  func(process.argv.slice(3));
+} else {
+  console.log(
+    "Hello! I am Sabestian, you're personal butler!\nUse 'list' command to see all available functions commands."
+  );
+}
