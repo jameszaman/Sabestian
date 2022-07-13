7bb714555e6e5f1f112f05aeffc692efe9f82e57
jameszaman <james.hedayet@gmail.com>
2022-07-14 00:01:30 +0600

    BugFix: Closing minecraft mouse click is now working.

--- python/mouse_press.py
+++ python/mouse_press.py
@@ -2,6 +2,7 @@ import mouse
 from time import time, sleep
 
 is_clicked = False
+run_program = True
 
 def click_handler():
   global is_clicked
@@ -11,10 +12,15 @@ def click_handler():
     mouse.hold()
     is_clicked = True
 
+def right_click_handler():
+  global run_program
+  run_program = False
 
 def main_event():
-  mouse.on_right_click(exit)
+  mouse.on_right_click(right_click_handler)
   mouse.on_click(click_handler)
-  sleep(36000)
+  while run_program:
+    sleep(1)
+  return
 
 main_event()
